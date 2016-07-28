3661f0b0b4ee5110551b99d985393530696259d2
Christopher Schuster <cs@livoris.net>
2016-07-28 14:00:17 -0700

    expose memDB

--- browserify.js
+++ browserify.js
@@ -3,4 +3,8 @@ var browserify = require('browserify');
 var b = browserify(
   ["jsgit-index.js"],
   {standalone: "jsgit"}
-).bundle().pipe(fs.createWriteStream("jsgit.js"));
+).bundle((err, buf) => {
+  var rep = "$1\n  repo.mem = {objects: objects, refs: refs};";
+  var rewritten = String(buf).replace(/(var refs = {};\n)/, rep);
+  fs.writeFileSync("jsgit.js", rewritten);
+});
